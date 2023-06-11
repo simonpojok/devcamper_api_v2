22a7f19a8ae9f6d2cd999171544027b7d7b5eff9
bigmosi <kinyeramo@gmail.com>
2023-06-11 15:19:20 +0300

    created controllers

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+const bootcamps = require('./routes/bootcamps');
+
 // Load env vars
 dotenv.config({ path: './config/config.env'});
 
 
 const app = express();
 
+app.use('/api/v1/bootcamps', bootcamps);
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(5000, (req, res) => {
     console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-})
+});
