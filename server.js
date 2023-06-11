02d3b18b8bdf5abe1def6c05fb4bf6c488f95152
bigmosi <kinyeramo@gmail.com>
2023-06-11 13:25:09 +0300

    setting up development environment

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env'});
+
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, (req, res) => {
+    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+})
