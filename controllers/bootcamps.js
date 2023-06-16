876d81d73d5cac7790002d60bd296418f63cb257
bigmosi <kinyeramo@gmail.com>
2023-06-16 11:21:15 +0300

    handle query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,11 +3,43 @@ const geocoder = require('../utils/geocoder');
 const asyncHandler = require('../middleware/async');
 
 const Bootcamp = require('../models/Bootcamp');
+const { param } = require('express/lib/request');
 // @desc  Get all bootcamps
 // @route GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-    const bootcamps = await Bootcamp.find();
+  let query;
+
+  // Copy req.query
+
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude
+  const removeFields = ['select'];
+
+  // Loop over removeFields and delete them from reqQuery
+
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  console.log(reqQuery);
+
+  // Create query string
+
+  let queryStr = JSON.stringify(reqQuery);
+  // Create operators ($gt, $gte, etc)
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  // Finding resource
+   query = Bootcamp.find(JSON.parse(queryStr));
+
+   // Select Fields
+   if (req.query.select) {
+    const fields = req.query.select.split(', ').join(' ');
+
+    query = query.select(fields);
+   }
+   // Excuting query
+    const bootcamps = await query;
     res.status(200).json({success: true, count: bootcamps.length, data: bootcamps});
 });
 
