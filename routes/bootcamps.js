20dbf71303a3043e460dd3dea84770cd118c1777
bigmosi <kinyeramo@gmail.com>
2023-07-08 23:03:05 +0300

    added registartion

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -24,7 +24,7 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
-router.route('/').get(advancedResults(Bootcampm, 'courses'), getBootcamps).post(createBootcamp);
+router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(createBootcamp);
 
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
