7d92e76b7dbbc662de2bcc20555af11114653d6b
bigmosi <kinyeramo@gmail.com>
2023-07-11 23:52:10 +0300

    run validator

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -51,10 +51,7 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @route PUT /api/v1/bootcamps/:id
 // @access Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-      const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    });
+      let bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
       return  next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
@@ -65,6 +62,12 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
     if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
       return next(new ErrorResponse(`User ${req.params.id} is not authorize to update the bootcamp`, 401));
     }
+
+    bootcamp =  await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    
     res.status(201).json({ success: true, data: bootcamp });
 });
 
