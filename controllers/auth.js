c3acc78b9e5d9578db1589fa5cdfa99cb6c1db7c
bigmosi <kinyeramo@gmail.com>
2023-07-25 11:49:49 +0300

    done with user reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -83,7 +83,7 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     await user.save({ validateBeforeSave: false })
 
     //Create reset url
-    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/resetpassword/${resetToken}`;
 
     const message = `You are receiving this email because you (or someone else) has requested the reset of a password.
     Please make a PUT request to: \n\n ${resetUrl}`;
@@ -132,11 +132,14 @@ exports.resetPassword = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse('Invalid token', 400));
     }
 
-    res.status(200).json({
-        success: true,
-        data: user,
-    })
+    // Set new password
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
 
+    await user.save();
+
+    sendTokenResponse(user, 200, res);
 });
 
 // Get token from model, create cookie and send response
